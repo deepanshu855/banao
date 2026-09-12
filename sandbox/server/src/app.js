60767a7890aa210634a947f1dc85f2ba1d8b69a8
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-09-12 14:44:05 +0530

    sandbox-server

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,21 +1,15 @@
-import express, { urlencoded } from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
+// Middleware
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res)=>{
-    res.send("Hello World")
-})
-
-app.get("/api/sandbox/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-    message: "This is the health route"
-  });
+// Routes
+app.get('/api/sandbox/health', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'Sandboxserver is healthy' });
 });
 
 export default app;
